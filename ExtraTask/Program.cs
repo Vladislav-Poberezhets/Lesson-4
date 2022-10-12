aa7fddcdce8753e91d8af01bebbcafa869dc6d2a
Vladislav-Poberezhets <vladpoberezhets1@gmail.com>
2022-10-12 21:29:08 +0300

    Add project files.

---
+++ ExtraTask/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введіть строку!");
+string str = Console.ReadLine();
+Console.WriteLine("Введіть кількість строк!");
+int num = int.Parse(Console.ReadLine());
+Method(str, num);
+
+void Method(string str, int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"{str}_");
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Lessont4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Введите первое число!");
+            string str1 = Console.ReadLine();
+            int.TryParse(str1, out int result1);
+
+            Console.WriteLine("Введите второе число!");
+            string str2 = Console.ReadLine();
+            int.TryParse(str2, out int result2);
+
+            MaxValue(result1, result2);
+
+            MinValue(result1, result2);
+
+            TrySumIfOdd(5, 4, out sum);
+        }
+        static void MaxValue(int a, int b)
+        {
+            if (a > b)
+                Console.WriteLine(a);
+        }
+
+        static void MinValue(int a, int b)
+        {
+            if (a < b)
+                Console.WriteLine(b);
+        }
+
+        static bool TrySumIfOdd(int a, int b, out int sum)
+        {
+
+            sum = a + b;
+            if (sum % 2 == 0)
+                return true;
+            else
+                Console.WriteLine($"Сума чисел {sum}");
+            return false;
+
+
+        }
+        static void MaxValue(int a, int b, int c) { }
+        static void MaxValue(int a, int b, int c, int x) { }
+        static void MinValue(int a, int b, int c) { }
+        static void MinValue(int a, int b, int c, int x) { }
+
+
+
+
+    }
+
+
+
+}
+
+
+
